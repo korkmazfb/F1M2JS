b4e5a7c25a905258c6d46e2bdd0f2d218d348e0a
korkmazfb <32840@ma-web.nl>
2021-11-30 13:16:53 +0100

    les 2

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # fase 1 module 2 museum Online
+
 ## javascript
 
-[live view van de opdrachten](http://32840.hosts1.ma-cloud.nl/f1m2js/)
+[live view van de opdrachten van les 1 javascript](http://32840.hosts1.ma-cloud.nl/f1m2js/les1-background-color/)
+
+[live view van de opdrachten van les 2 javascript](http://32840.hosts1.ma-cloud.nl/f1m2js/les2-boodschappen/)
--- web/les2-boodschappen/script.js
+++ web/les2-boodschappen/script.js
@@ -1,1 +1,23 @@
 console.log("script is geladen!")
+
+const groentevak = document.getElementById('groente');
+const fruitvak = document.getElementById('fruit'); 
+
+function zetinFruitla(fruit){
+    console.log(fruit);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = fruit;
+    nieuwElement.className = 'rood';
+    fruitvak.appendChild(nieuwElement);
+
+}
+
+function zetinGroentela(groente){
+    console.log(groente);
+
+    const nieuwElement = document.createElement('h2');
+    nieuwElement.innerHTML = groente;
+    nieuwElement.className = 'groen';
+    groentevak.appendChild(nieuwElement);
+}
