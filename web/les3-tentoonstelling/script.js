855a79ecafb9f9e1f3ae21bc7ad40acedc257b85
korkmazfb <32840@ma-web.nl>
2021-12-10 10:33:58 +0100

    les3

---
+++ web/les3-tentoonstelling/script.js
@@ -0,0 +1,28 @@
+const myImage = document.getElementById('myImage');
+const myTitle = document.getElementById('myTitle');
+
+let paintings = ["img/painting0.jpg",
+"img/painting1.jpg",
+"img/painting2.jpg",
+"img/painting3.jpg",
+"img/painting4.jpg",
+"img/painting5.jpg"
+
+];
+
+let titles = [
+
+    "de schilderijen van van Gogh",
+    "De rode wijngaard",
+    "De sterrennacht",
+    "Sterrennacht boven de Rh�ne",
+    "Zonnebloemen",
+    "Boerderij in de Provence"
+];
+
+function changeImage(index){
+    myTitle.innerHTML = titles[index]
+
+    myImage.src = paintings[index];
+}
+
